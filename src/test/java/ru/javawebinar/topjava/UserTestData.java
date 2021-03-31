80d6a0c8ec4179632025d5ba1c6f47a50b114c0f
Savadevel <savadevel@gmail.com>
2021-03-31 13:23:10 +0300

    fix: after code review

--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -8,16 +8,17 @@ import java.util.Collections;
 import java.util.Date;
 
 import static ru.javawebinar.topjava.model.AbstractBaseEntity.START_SEQ;
+import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class UserTestData {
     public static final TestMatcher<User> USER_MATCHER = TestMatcher.usingIgnoringFieldsComparator(User.class, "registered", "meals");
 
     public static final int USER_ID = START_SEQ;
     public static final int ADMIN_ID = START_SEQ + 1;
-    public static final int AUTH_USER_ID = SecurityUtil.authUserId();
+    public static final int AUTH_USER_ID = USER_ID;
     public static final int NOT_FOUND = 10;
 
-    public static final int AUTH_USER_CALORIES_PER_DAY = SecurityUtil.authUserCaloriesPerDay();
+    public static final int AUTH_USER_CALORIES_PER_DAY = DEFAULT_CALORIES_PER_DAY;
 
     public static final User user = new User(USER_ID, "User", "user@yandex.ru", "password", Role.USER);
     public static final User admin = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ADMIN, Role.USER);
